eda14b36441a18fdd20ff97c86873d22389718c3
zerg959 <zerg959@yandex.ru>
2024-04-23 15:11:15 +0300

    NEW: edit post page, login page, logic

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import sqlite3
 
 app = Flask(__name__)
@@ -44,26 +44,61 @@ def new_post():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
 
-        conn = get_db_connection()
-        conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
-        conn.commit()
-        conn.close()
-
-        return redirect(url_for('index'))
+            return redirect(url_for('index'))
 
     return render_template('add_post.html')
 
-# @app.route("/<int:post_id>/delete/", methods=['POST',])
-# # @login_required
-# def post_del(post_id):
-#     post = get_post(id)
-#     conn = get_db_connection()
-#     conn.execute('DELETE FROM posts WHERE id = ?', (id,))
-#     conn.commit()
-#     conn.close()
-#     # flash('"{}" was successfully deleted!'.format(post['title']))
-#     return redirect(url_for('index'))
+
+@app.route('/<int:post_id>/edit', methods=('GET', 'POST'))
+def edit(post_id):
+    post = get_post(post_id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        # if not title:
+        #     flash('Title is required!')
+        # else:
+        conn = get_db_connection()
+        conn.execute('UPDATE posts SET title = ?, content = ?'
+                    ' WHERE id = ?',
+                    (title, content, post_id))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
+
+
+@app.route("/<int:post_id>/delete/", methods=('POST',))
+def post_del(post_id):
+    # post = get_post(post_id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (post_id,))
+    conn.commit()
+    conn.close()
+    # flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # проверка логина и пароля
+        return 'Вы вошли в систему!'
+    else:
+        return render_template('login.html')
 
 @app.before_first_request
 def before_first_request():
